291f5052ce74d6da2b19c0f6252deb3a4d9db742
Carson Davis <CarsonReidDavis@gmail.com>
2025-03-10 16:15:30 -0500

    relabel cleaning prompts

--- utils/constants.py
+++ utils/constants.py
@@ -30,7 +30,7 @@ DEFAULT_TRANSLATE_MODEL = "openai:gpt-4o"
 DEFAULT_TEMPERATURE = 0.75
 AVAILABLE_MODELS = ["openai:gpt-4o", "anthropic:claude-3-5-sonnet-20240620"]
 
-# ocr defaults
+# OCR defaults
 DEFAULT_PROCESS_IMAGES = True
 
 # Process defaults
@@ -38,17 +38,7 @@ DEFAULT_PIPELINE_DIR = DEFAULT_OUTPUT_PARENT  # Use the same parent directory
 DEFAULT_SKIP_CLEAN = False
 DEFAULT_SKIP_TRANSLATE = False
 
-# Translation Prompts
-TRANSLATION_SYSTEM_PROMPT = (
-    "You are a professional translator specializing in Old French to modern English"
-)
-TRANSLATION_USER_PROMPT = (
-    "Translate the following Old French text to modern English. Maintain the markdown "
-    "formatting of the original. Ensure the translation captures both the meaning and "
-    "the style of the original text."
-)
-
-# Translation Prompts
+# Cleaning Prompts
 CLEANING_SYSTEM_PROMPT = "You are a deliberate and careful editor of old French"
 CLEANING_USER_PROMPT = """You will be given an OCR-generated transcription of the 16th-century French legal text **"Arrest mémorable du Parlement de Tolose"** by Jean de Coras, a detailed account of the Martin Guerre impostor case. This OCR transcription contains transcription errors such as incorrect character recognition, misplaced punctuation, and spacing mistakes.
 **Your task is to:**
@@ -62,3 +52,13 @@ CLEANING_USER_PROMPT = """You will be given an OCR-generated transcription of th
 
 Begin now.
 """
+
+# Translation Prompts
+TRANSLATION_SYSTEM_PROMPT = (
+    "You are a professional translator specializing in Old French to modern English"
+)
+TRANSLATION_USER_PROMPT = (
+    "Translate the following Old French text to modern English. Maintain the markdown "
+    "formatting of the original. Ensure the translation captures both the meaning and "
+    "the style of the original text."
+)
